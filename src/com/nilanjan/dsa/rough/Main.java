4eea8baae60e78fdd50155338ce8a12e4ddba7a3
Nilanjan Bhattacharjee <nilanjan.bhattacharjee77@gmail.com>
2022-01-12 23:29:04 +0530

    modified struct

--- src/com/nilanjan/dsa/rough/Main.java
+++ src/com/nilanjan/dsa/rough/Main.java
@@ -1,10 +1,7 @@
 package com.nilanjan.dsa.rough;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello Java");
-        System.out.println();
+        System.out.println("hello");
     }
 }
--- src/com/nilanjan/dsa/rough/Simple.java
+++
@@ -1,7 +0,0 @@
-package com.nilanjan.dsa.rough;
-
-class GFG{
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
-} 
