0f0b80e002e73ad6562f48fcbc1ae557dd9bedb9
Nilanjan Bhattacharjee <nilanjan.bhattacharjee77@gmail.com>
2022-01-12 23:12:48 +0530

    init

---
+++ src/com/nilanjan/dsa/Main.java
@@ -0,0 +1,10 @@
+package com.nilanjan.dsa;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Java");
+        System.out.println();
+    }
+}
---
+++ src/com/nilanjan/dsa/Simple.java
@@ -0,0 +1,7 @@
+package com.nilanjan.dsa;
+
+class GFG{
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+} 
