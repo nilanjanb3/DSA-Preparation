517a0ff61f3f7e3fb8c1b1470224c97387fee44b
Nilanjan Bhattacharjee <nilanjan.bhattacharjee77@gmail.com>
2022-01-17 22:11:09 +0530

    added QuickSort.java

--- src/com/nilanjan/dsa/rough/Main.java
+++ src/com/nilanjan/dsa/rough/Main.java
@@ -1,7 +1,21 @@
 package com.nilanjan.dsa.rough;
 
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        TreeSet<Integer> s = new TreeSet<>();
+        s.add(100);
+        s.add(200);
+        s.add(300);
+        s.add(400);
+
+        SortedSet<Integer> res;
+
+        res = s.tailSet(200);
+
+        System.out.println(res.toString());
+
     }
 }
